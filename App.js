ac75303d1db50f0a8440704c57264bef82d08eca
hijakd <jharyd@gmail.com>
2021-03-07 09:58:19 +1100

    file management, create basic WelcomeScreen

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import {StyleSheet, Text } from 'react-native';
 import React from 'react';
-import AppScreen from './app/components/AppScreen';
+import AppScreen from './app/assets/screens/AppScreen';
 
 export default function App() {
   return (
---
+++ app/assets/screens/WelcomeScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {StyleSheet, Text } from 'react-native'
+import AppScreen from './AppScreen';
+
+function WelcomeScreen(props) {
+    return (
+        <AppScreen>
+            
+        </AppScreen>
+    );
+}
+
+const styles = StyleSheet.create({
+    
+})
+
+export default WelcomeScreen;
