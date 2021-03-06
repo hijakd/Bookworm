f6fbf164d3675d064837d096aa2d69c226160059
hijakd <jharyd@gmail.com>
2021-03-06 20:30:32 +1100

    edit AppButton add padding & alignment/justification

--- app/components/AppButton.js
+++ app/components/AppButton.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import {Text, StyleSheet, TouchableOpacity, View } from 'react-native';
+import {Text, StyleSheet, TouchableOpacity, View, TouchableOpacityBase } from 'react-native';
 import AppColors from '../configs/AppColors';
 
 function AppButton({title}) {
     return (
-        <View style={styles.button}>
-            <Text>{title}</Text>
-        </View>
+        <TouchableOpacity>
+            <View style={styles.button}>
+                <Text> {title} </Text>
+            </View>
+        </TouchableOpacity>
     );
 }
 
 const styles = StyleSheet.create({
     button:{
         backgroundColor: AppColors.secondaryColor,
+        borderRadius: 20,
+        width: '100%',
+        padding: 15,
+        justifyContent: 'center',
+        alignItems: 'center',
     }
 })
 
