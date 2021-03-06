43943f77470836f7c003a1bc38db1f5b1952843b
hijakd <jharyd@gmail.com>
2021-03-06 20:14:51 +1100

    create basic AppButton component

---
+++ app/components/AppButton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Text, StyleSheet, TouchableOpacity, View } from 'react-native';
+
+function AppButton(props) {
+    return (
+        <View style={styles.button}>
+            <Text></Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    button:{
+        
+    }
+})
+export default AppButton;
