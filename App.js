8055a5513361467def2c7d6f8d28eb840e36265a
hijakd <jharyd@gmail.com>
2021-03-06 20:00:27 +1100

    Import & impliment AppText to style text

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import AppText from './app/components/AppText';
 import AppColors from './app/configs/AppColors';
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> Hello Cactus!?!</Text>
+      <AppText>Hello Cactus</AppText>
     </View>
   );
 }
