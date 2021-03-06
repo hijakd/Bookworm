ad6ff566fea7f7e47f0d454f627af8f84557f16f
hijakd <jharyd@gmail.com>
2021-03-06 22:54:26 +1100

    implement background image

--- App.js
+++ App.js
@@ -1,29 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Platform } from 'react-native';
 import {MaterialCommunityIcons} from '@expo/vector-icons';
 import AppButton from './app/components/AppButton';
 import AppText from './app/components/AppText';
 import AppColors from './app/configs/AppColors';
 
+const blurRadiusValue = Platform.OS === "android" ? 0.7 : 5.5;
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <MaterialCommunityIcons 
-        name = "library"
-        size = {68}
-        color = {AppColors.primaryColor}
-      />
-    </View>
+    <ImageBackground 
+      source = {require("./app/assets/background.jpg")}
+      style = {styles.background}
+      blurRadius = {0.6}
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  // container: {
+  //   flex: 1,
+  //   // using a defined colour palette "AppColor.js" in ./app/configs
+  //   backgroundColor: AppColors.otherColor,
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
+  background: {
     flex: 1,
-    // using a defined colour palette "AppColor.js" in ./app/configs
-    backgroundColor: AppColors.otherColor,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
