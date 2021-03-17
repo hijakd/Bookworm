553e588ed5a722e9f9ff4436273e88a5bb6c6ecd
hijakd <jharyd@gmail.com>
2021-03-17 16:30:55 +1100

    create RegisterScreen using State variables

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
-import {ImageBackground, StyleSheet, Text } from 'react-native';
 import React from 'react';
-import AppScreen from './app/assets/screens/AppScreen';
+import {ImageBackground, StyleSheet, Text } from 'react-native';
+
 import WelcomeScreen from './app/assets/screens/WelcomeScreen';
 import LoginScreen from './app/assets/screens/LoginScreen';
+import RegisterScreen from './app/assets/screens/RegisterScreen';
+import AppButton from './app/assets/components/AppButton';
+import AppScreen from './app/assets/screens/AppScreen';
 import AppStyles from './app/assets/configs/AppStyles';
 
 
 export default function App() {
   return (
-    <LoginScreen/>  
+    <RegisterScreen/>  
     
 
   );
--- app/assets/screens/RegisterScreen.js
+++ app/assets/screens/RegisterScreen.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import {MaterialCommunityIcons} from '@expo/vector-icons'
 
@@ -6,19 +6,70 @@ import {MaterialCommunityIcons} from '@expo/vector-icons'
 import AppTextInput from '../components/AppTextInput'
 import AppScreen from './AppScreen'
 import AppColors from '../configs/AppColors'
+import AppButton from '../components/AppButton'
 
 
-AppTextInput
-function RegisterScreen() {
+
+function RegisterScreen(props) {
+    const [userName, setUserName] = useState();
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
     return (
-        
-        
-        <View>
-            <Text></Text>
+        <AppScreen style={styles.container}>
+        <View style={styles.welcomeContainer}>
+            <MaterialCommunityIcons 
+                name="library"
+                size={68}
+                color={AppColors.primaryColor}/>
         </View>
-    )
+        <View style={styles.textInputContainer}>
+            <AppTextInput
+                autoCapitalize= "words"
+                autoCorrect= {false}
+                icon= "account"
+                placeholder= "Full Name"
+                textContentType= "emailAddress"
+                onChangeText= {userInput => setUserName(userInput)}
+            />  
+            <AppTextInput
+                autoCapitalize= "none"
+                autoCorrect= {false}
+                icon= "email"
+                placeholder= "Email Address"
+                keyboardType= "email-address"
+                textContentType= "emailAddress"
+                onChangeText= {userInput => setEmail(userInput)}
+            />
+            <AppTextInput
+                autoCapitalize= "none"
+                autoCorrect= {false}
+                icon= "lock"
+                placeholder= "Password"
+                secureTextEntry  // this sets secureTextEntry to true
+                textContentType= "password"
+                onChangeText= {userInput => setPassword(userInput)}
+            />                
+        </View>
+        <AppButton title="Login" onPress={() => console.log(userName, email, password)}/>
+    </AppScreen>   
+);
 }
 
 const styles = StyleSheet.create({
-    style
+    container:{
+        flex:1,
+        backgroundColor:AppColors.otherColor,
+        padding: 25, // for SafeAreaView check OS in AppScreen and pad there for iOS
+    },
+    welcomeContainer:{
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 50,
+    },
+    textInputContainer:{
+        marginTop: 50,
+        marginBottom: 30,
+    }
 })
+
+export default RegisterScreen;
