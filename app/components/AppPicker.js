c65dff4966d42275d3d58b9cec5b0a6e710300d8
hijakd <jharyd@gmail.com>
2021-03-29 21:21:00 +1100

    make Picker clickable & animated

--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -1,17 +1,29 @@
-import React from "react";
-import { View, StyleSheet, TextInput, Text } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, TextInput, Text, Modal, Button } from "react-native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import AppStyles from "../configs/AppStyles";
 import AppText from "./AppText";
 import AppColors from "../configs/AppColors";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
+import AppScreen from "./AppScreen";
 
 function AppPicker({ icon, placeholder, ...otherProps }) {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
-    <View style={AppStyles.pickerContainer}>
-      <MaterialCommunityIcons name={icon} size={28} />
-      <AppText style={AppStyles.pickerText}>{placeholder}</AppText>
-      <MaterialCommunityIcons name="chevron-down" size={28} />
-    </View>
+    <React.Fragment>
+      <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
+        <View style={AppStyles.pickerContainer}>
+          {icon && <MaterialCommunityIcons name={icon} size={28} />}
+          <AppText style={AppStyles.pickerText}>{placeholder}</AppText>
+          <MaterialCommunityIcons name="chevron-down" size={28} />
+        </View>
+      </TouchableWithoutFeedback>
+      <Modal visible={modalVisible} animationType="slide">
+        <AppScreen>
+          <Button title="Close" onPress={() => setModalVisible(false)} />
+        </AppScreen>
+      </Modal>
+    </React.Fragment>
   );
 }
 
