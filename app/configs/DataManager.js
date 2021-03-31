7c878d58fde392eed84ed3898889d27b59d22705
hijakd <jharyd@gmail.com>
2021-03-31 18:14:09 +1100

    create book data within DataManager

--- app/configs/DataManager.js
+++ app/configs/DataManager.js
@@ -2,6 +2,33 @@ export default class DataManager {
   static myInstance = null;
   userID = "";
 
+  books = [
+    {
+      userid: "user1",
+      bookid: 1,
+      title: "Harry Potter",
+      subtitle: "read on the train",
+      image: require("../assets/images/Book2Cover.jpeg"),
+      category: "Fiction",
+    },
+    {
+      userid: "user1",
+      bookid: 2,
+      title: "Game of Thrones",
+      subtitle: "read on the bus",
+      image: require("../assets/images/Book1Cover.jpg"),
+      category: "Fiction",
+    },
+    {
+      userid: "user2",
+      bookid: 3,
+      title: "Harry Potter",
+      subtitle: "read on the train",
+      image: require("../assets/images/Book2Cover.jpeg"),
+      category: "Fiction",
+    },
+  ];
+
   static getInstance() {
     if (DataManager.myInstance == null) {
       DataManager.myInstance = new DataManager();
@@ -15,5 +42,9 @@ export default class DataManager {
 
   setUserID(id) {
     this.userID = id;
+  }
+
+  getBooks(id) {
+    return this.books.filter((book) => book.userid === id);
   }
 }
